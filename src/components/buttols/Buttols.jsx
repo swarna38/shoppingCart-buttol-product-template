19ce615a6be46fa958ba796d0061abd9c64fb8b0
swarna38 <swarna.sutradhar.info@gamil.com>
2026-07-27 02:37:44 +0600

    cart product show working carts.map

--- src/components/buttols/Buttols.jsx
+++ src/components/buttols/Buttols.jsx
@@ -7,7 +7,7 @@ const Buttols = () => {
     const [buttols, setButtols] = useState([]); 
 
     //cart variable
-    const [cart, setCart] = useState([]);
+    const [carts, setCart] = useState([]);
 
     useEffect(() => {
         fetch('buttols.json')
@@ -16,14 +16,25 @@ const Buttols = () => {
     },[])
 
     const handelAddToCart = buttol => {
-        const newCart = [...cart, buttol];
+        const newCart = [...carts, buttol];
         setCart(newCart)
     }
 
     return (
         <div>
             <h3>product :{buttols.length}</h3>
-            <h4>cart: {cart.length}</h4>
+            <h4>carts: {carts.length}</h4>
+
+            <div className="cart-container">
+                {
+                    carts.map((cart, index) => (
+                      <div key={index}>
+                        <img src={cart.image} alt={cart.title} width="50" />
+                        <p>{cart.title}</p>
+                        </div>
+                    ))
+                }
+            </div>
 
             <div className="buttols-container">
                 {
